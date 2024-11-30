788a36af4c3722acadbac3c3d340edc96f17c452
Doan Thanh Sang <sangthanh1110@gmail.com>
2024-11-30 17:12:13 +0700

    change color popup

--- index.php
+++ index.php
@@ -47,11 +47,11 @@
             <h2><span class="yellow">SCHEDULE</span> A FREE <br> X-RAY AND DENTAL CHECK-UP</h2>
             <form class="form-container" method="POST" action="process.php"> <input type="text" name="name" placeholder="Your Name" required> <input type="text" name="phone" placeholder="Phone number" required> <input type="email" name="email" placeholder="Your Email" required> <input type="text" name="country" placeholder="Your Current Country" required> <select name="app" required>
                     <option value="" disabled selected>Your Available App</option>
-                    <option value="WhatsApp">WhatsApp</option>
-                    <option value="Viber">Viber</option>
-                    <option value="Skype">Skype</option>
-                    <option value="Zoom">Zoom</option>
-                    <option value="Teams">Teams</option>
+                    <option style="color:black" value="WhatsApp">WhatsApp</option>
+                    <option style="color:black" value="Viber">Viber</option>
+                    <option style="color:black" value="Skype">Skype</option>
+                    <option style="color:black" value="Zoom">Zoom</option>
+                    <option style="color:black" value="Teams">Teams</option>
                 </select> <button type="submit">REGISTER NOW</button> </form>
         </div>
     </div>
@@ -156,33 +156,33 @@
                 class="font-size-3"> 5 GIÁ TRỊ CỐT LÕI TẠI NHA KHOA VIỆT HÀN</b></h3>
         <div style="font-family: Arial, Helvetica, sans-serif;" class="col-11 d-flex d-inline-block justify-content-between p-1 text-center mx-auto">
             <div class="col-8 col-md-2 text-center mt-3 border border-white p-3 shadow wow fadeInUp cl">
-                <h6>Chuyên môn hàng đầu</h6>
+                <h5>Chuyên môn hàng đầu</h5>
                 <p style="text-align: justify;">Chúng tôi tự hào vì 100% bác sĩ tại Nha Khoa Việt Hàn đều là những
                     chuyên gia chính quy chuyên khoa Răng
                     Hàm Mặt. Điều này đảm bảo rằng mỗi dịch vụ chúng tôi cung cấp đều đạt tiêu chuẩn y tế cao nhất, mang
-                    lại
+                    lạiD
                     sự an tâm cho khách hàng.</p>
             </div>
             <div class="col-8 col-md-2 text-center mt-3 border border-white p-3 shadow wow fadeInUp cl">
-                <h6>Sức khỏe bệnh nhân là ưu tiên tuyệt đối</h6>
+                <h5>Sức khỏe bệnh nhân là ưu tiên tuyệt đối</h5>
                 <p style="text-align: justify;">Chúng tôi luôn đặt sức khỏe và sự an toàn của bệnh nhân lên hàng đầu.
                     Mọi quyết định, mọi hành động đều
                     hướng tới việc mang lại giải pháp điều trị tốt nhất cho khách hàng.</p>
             </div>
             <div class="col-8 col-md-2 text-center mt-3 border border-white p-3 shadow wow fadeInUp cl">
-                <h6>Tận tâm và trung thực</h6>
+                <h5>Tận tâm và trung thực</h5>
                 <p style="text-align: justify;">Chúng tôi cam kết phục vụ khách hàng với sự chân thành, minh bạch. Chăm
                     sóc khách hàng như chính người
                     thân trong gia đình mình.</p>
             </div>
             <div class="col-8 col-md-2 text-center mt-3 border border-white p-3 shadow wow fadeInUp cl">
-                <h6>Luôn đổi mới và phát triển</h6>
+                <h5>Luôn đổi mới và phát triển</h5>
                 <p style="text-align: justify;">Chúng tôi không ngừng học hỏi, cải tiến trang thiết bị và công nghệ.
                     Nâng cao năng lực bản thân để đáp
                     ứng nhu cầu ngày càng cao của khách hàng và thị trường.</p>
             </div>
             <div class="col-8 col-md-2 text-center mt-3 border border-white p-3 shadow wow fadeInUp cl">
-                <h6>Đề cao trách nhiệm xã hội </h6>
+                <h5>Đề cao trách nhiệm xã hội </h5>
                 <p style="text-align: justify;">
                     Chúng tôi tự hào khi được đóng góp tích cực vào cộng đồng và xã hội. Bằng việc thực hiện các hoạt
                     động
