5f52f7b2374c36eefc23986ed3184ab4e403da71
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-12-13 14:10:16 +0700

    update

--- view/info/implant.php
+++ view/info/implant.php
@@ -5,5 +5,5 @@
 </style>
 <div class="col-12 text-center">
     <img class="img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp" src="../../asset/images/implant.jpg" alt="">
-    <img class="img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp mt-lg-4 mt-2" src="../../asset/images/implant2.jpg" alt="">
+    <img class="img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp mt-lg-5 mt-3" src="../../asset/images/implant2.jpg" alt="">
 </div>
