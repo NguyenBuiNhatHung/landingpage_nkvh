caba7e46e999af8193c180f34267b793f4abcfb4
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-12-16 12:59:47 +0700

    up

--- view/info/cotloi.php
+++ view/info/cotloi.php
@@ -8,12 +8,17 @@
     }
     .cl{
         color:rgb(0, 166, 218);
-        min-height: 370px;
+        min-height: 270px;
         border-radius: 2%;
         box-shadow: 0 4px 15px rgba(255, 255, 255, 0.8);
     }
     .owl-cl{
         margin-top: -16px;
+    }
+    @media screen and (max-width: 1700px) {
+        .cl{
+            min-height: 370px;
+        }
     }
     @media screen and (max-width: 768px) {
         #text-cl {
@@ -39,33 +44,35 @@
         .nk-title-cl{
             font-size: 20px;
         }
-
+    }
+    .nk-font-weight-700{
+        font-weight: 700;
     }
 </style>
 <div class="text-center col-12 wow animate__animated animate__fadeInLeft">
     <p id="text-cl">5 GIÁ TRỊ CỐT LÕI TẠI NHA KHOA VIỆT HÀN</p>
     <div class="owl-carousel owl-cl col-10 mx-auto">
         <div class="cl p-4 border bg-white shadow custom-margin">
-            <h5 class="nk-title-cl">1. Chuyên môn hàng đầu</h5>
+            <h5 class="nk-title-cl nk-font-weight-700">1. Chuyên môn hàng đầu</h5>
             Chúng tôi tự hào vì 100% bác sĩ tại Nha Khoa Việt Hàn đều là những
                 chuyên gia chính quy chuyên khoa Răng Hàm Mặt. Điều này đảm bảo rằng mỗi dịch vụ chúng tôi cung cấp đều đạt tiêu chuẩn y tế cao nhất, mang lại sự an tâm cho khách hàng.
         </div>
         <div class="cl p-4 border bg-white shadow custom-margin">
-            <h5 class="nk-title-cl">2. Sức khỏe bệnh nhân là ưu tiên tuyệt đối</h5>
+            <h5 class="nk-title-cl nk-font-weight-700">2. Sức khỏe bệnh nhân là ưu tiên tuyệt đối</h5>
             Chúng tôi luôn đặt sức khỏe và sự an toàn của bệnh nhân lên hàng đầu.
                 Mọi quyết định, mọi hành động đều hướng tới việc mang lại giải pháp điều trị tốt nhất cho khách hàng.
         </div>
         <div class="cl p-4 border bg-white shadow custom-margin">
-            <h5 class="nk-title-cl">3. Tận tâm và trung thực</h5>
+            <h5 class="nk-title-cl nk-font-weight-700">3. Tận tâm và trung thực</h5>
             Chúng tôi cam kết phục vụ khách hàng với sự chân thành, minh bạch. Chăm sóc khách hàng như chính người thân trong gia đình mình.
         </div>
         <div class="cl p-4 border bg-white shadow custom-margin">
-            <h5 class="nk-title-cl">4. Luôn đổi mới và phát triển</h5>
+            <h5 class="nk-title-cl nk-font-weight-700">4. Luôn đổi mới và phát triển</h5>
             Chúng tôi không ngừng học hỏi, cải tiến trang thiết bị và công nghệ.
                 Nâng cao năng lực bản thân để đáp ứng nhu cầu ngày càng cao của khách hàng và thị trường.
         </div>
         <div class="cl p-4 border bg-white shadow custom-margin">
-            <h5 class="nk-title-cl">5. Đề cao trách nhiệm xã hội</h5>
+            <h5 class="nk-title-cl nk-font-weight-700">5. Đề cao trách nhiệm xã hội</h5>
            Chúng tôi tự hào khi được đóng góp tích cực vào cộng đồng và xã hội. Bằng việc thực hiện các hoạt động
                 thiện nguyện, chia sẻ yêu thương với những hoàn cảnh khó khăn.
         </div>
