f1150232b3df1e051270a9fd082a81380873df24
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-12-17 10:31:23 +0700

    up

--- view/info/implant.php
+++ view/info/implant.php
@@ -11,6 +11,6 @@
     }
 </style>
 <div id="implant" class="nk-bg-color-1 col-12 text-center">
-    <img class="img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp" src="../../asset/images/implant.jpg" alt="">
+    <img class="img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp" src="../../asset/images/implant2.svg" alt="">
     <img class="nk-mb-40 img-imp col-12 col-md-10 wow animate__animated animate__fadeInUp" src="../../asset/images/IMPLANTS-1.svg" alt="">
 </div>
