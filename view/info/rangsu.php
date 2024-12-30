a09887897fe2c93c3815a1bfef96dc6d5eb4221d
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-12-30 14:21:26 +0700

    fix rs1

--- view/info/rangsu.php
+++ view/info/rangsu.php
@@ -7,6 +7,6 @@
     }
 </style>
 <div id="rangsu" class="nk-bg-color-1 text-center">
-    <img class="rangsu img-rs wow animate__animated animate__fadeInUp" src="../../asset/images/rangsu1.png" alt="">
+    <img class="rangsu img-rs wow animate__animated animate__fadeInUp" src="../../asset/images/rangsu11.png" alt="">
     <img class="rangsu img-rs wow animate__animated animate__fadeInUp" src="../../asset/images/rangsu2.png" alt="">
 </div>
